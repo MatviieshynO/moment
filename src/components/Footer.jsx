62209f21d8f0d4b929c67fa4b26d9d28457e7d49
MatviieshynO <Olopromotion@gmail.com>
2023-04-29 18:33:42 +0200

    next step footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,14 +8,14 @@ export default function Footer() {
         <div className="mx-auto mt-5 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-2 sm:max-w-xl sm:grid-cols-2 sm:gap-x-2 lg:mx-0 lg:max-w-none lg:grid-cols-2 ">
           <Link
             to="/privacy"
-            className="col-span-2 max-h-7 w-full object-contain lg:col-span-1  text-black text-center border-b-[1px] border-black text-sm leading-8"
+            className="col-span-2 max-h-7 w-full object-contain lg:col-span-1  text-black text-center border-b-[1px] border-black text-xs leading-8"
           >
             Політика приватності
           </Link>
 
           <Link
             to="/legal"
-            className="col-span-2 max-h-7 w-full object-contain lg:col-span-1 text-black text-center border-b-[1px] border-black text-sm leading-8"
+            className="col-span-2 max-h-7 w-full object-contain lg:col-span-1 text-black text-center border-b-[1px] border-black text-xs leading-8"
           >
             Правова інформація
           </Link>
--- src/components/ScrollToTopButton.jsx
+++ src/components/ScrollToTopButton.jsx
@@ -24,7 +24,7 @@ const ScrollToTopButton = () => {
     <div className="fixed sm:bottom-[10%] sm:right-[3%] md:bottom-[10%] md:right-[3%] bottom-[-10px] right-[45%] text-6xl">
       {showScrollButton && (
         <KeyboardDoubleArrowUpIcon
-          className="cursor-pointer hover:border-[1px] bg-incherit hover:bg-white"
+          className="cursor-pointer bg-incherit"
           onClick={scrollToTop}
           fontSize="large"
         />
