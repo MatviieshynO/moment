47cefed3b44a2e05ab1da4358e1dacc54be85caa
MatviieshynO <Olopromotion@gmail.com>
2023-05-02 04:16:26 +0200

    fdsf

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -312,8 +312,8 @@ export default function Navbar() {
                             className="h-4 w-4"
                           >
                             <path
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
                               d="M19.5 8.25l-7.5 7.5-7.5-7.5"
                             />
                           </svg>
@@ -353,8 +353,8 @@ export default function Navbar() {
                           className="h-4 w-4"
                         >
                           <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
                             d="M19.5 8.25l-7.5 7.5-7.5-7.5"
                           />
                         </svg>
@@ -372,47 +372,7 @@ export default function Navbar() {
                   </div>
                 </div>
                 {/*  item3*/}
-                <div>
-                  <button
-                    className="group relative justify-center flex w-full items-center border-0 bg-white px-5 py-4 text-left text-base text-neutral-800 transition [overflow-anchor:none] hover:z-[2] focus:z-[3] focus:outline-none dark:bg-neutral-800 dark:text-white [&:not([data-te-collapse-collapsed])]:bg-white [&:not([data-te-collapse-collapsed])]:text-primary [&:not([data-te-collapse-collapsed])]:[box-shadow:inset_0_-1px_0_rgba(229,231,235)] dark:[&:not([data-te-collapse-collapsed])]:bg-neutral-800 dark:[&:not([data-te-collapse-collapsed])]:text-primary-400 dark:[&:not([data-te-collapse-collapsed])]:[box-shadow:inset_0_-1px_0_rgba(75,85,99)] [&[data-te-collapse-collapsed]]:rounded-b-[15px] [&[data-te-collapse-collapsed]]:transition-none"
-                    type="button"
-                    data-te-collapse-init
-                    data-te-collapse-collapsed
-                    data-te-target="#collapseFour"
-                    aria-expanded="false"
-                    aria-controls="collapseFour"
-                  >
-                    <div className="flex justify-center items-center">
-                      <span className="mr-2">Про нас</span>
-                      <span className="-mr-1 ml-auto h-5 w-5 shrink-0 rotate-[-180deg] fill-[#336dec] transition-transform duration-200 ease-in-out group-[[data-te-collapse-collapsed]]:mr-0 group-[[data-te-collapse-collapsed]]:rotate-0 group-[[data-te-collapse-collapsed]]:fill-[#212529] motion-reduce:transition-none dark:fill-blue-300 dark:group-[[data-te-collapse-collapsed]]:fill-white">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke-width="1.5"
-                          stroke="currentColor"
-                          className="h-4 w-4"
-                        >
-                          <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-                          />
-                        </svg>
-                      </span>
-                    </div>
-                  </button>
-
-                  <div
-                    id="collapseFour"
-                    className="!visible hidden flex justify-center"
-                    data-te-collapse-item
-                    aria-labelledby="headingFour"
-                  >
-                    <span>dasd</span>
-                  </div>
-                </div>
-
+                <Link to="/">Про нас</Link>
                 {/*  */}
               </div>
 
