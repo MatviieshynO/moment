40e71edc61a474fa19e963ce7b5449841a26463c
MatviieshynO <Olopromotion@gmail.com>
2023-05-05 01:58:23 +0200

    dad

--- src/components/ArrowSection.jsx
+++ src/components/ArrowSection.jsx
@@ -1,6 +1,7 @@
 export default function ArrowSection() {
   return (
     <div className="bg-white">
+      {/* arrowSection */}
       <div className="flex justify-center">
         <div className="bg-[url('../public/images/arrowImage2.gif')] h-[70px] w-40 bg-no-repeat bg-cover bg-center top-10"></div>
         <div className="flex items-center">
@@ -14,46 +15,15 @@ export default function ArrowSection() {
         </div>
         <div className="bg-[url('../public/images/arrowImage.gif')] h-[70px] w-40 bg-no-repeat bg-cover bg-center top-10"></div>
       </div>
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <h2 className="text-center text-lg font-semibold leading-8 text-gray-900">
-          Нашe меню
+      {/* /arrowSection */}
+      <div className=" pt-2 bg-[#D6EDF1] border-t-2 border-teal-300">
+        <h2 className="text-center text-lg leading-6 text-teal-800 font-mono pb-2">
+          У нас ви можете замовити
         </h2>
-        <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
-          <img
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-            src="https://tailwindui.com/img/logos/158x48/transistor-logo-gray-900.svg"
-            alt="Transistor"
-            width={158}
-            height={48}
-          />
-          <img
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-            src="https://tailwindui.com/img/logos/158x48/reform-logo-gray-900.svg"
-            alt="Reform"
-            width={158}
-            height={48}
-          />
-          <img
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-            src="https://tailwindui.com/img/logos/158x48/tuple-logo-gray-900.svg"
-            alt="Tuple"
-            width={158}
-            height={48}
-          />
-          <img
-            className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
-            src="https://tailwindui.com/img/logos/158x48/savvycal-logo-gray-900.svg"
-            alt="SavvyCal"
-            width={158}
-            height={48}
-          />
-          <img
-            className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
-            src="https://tailwindui.com/img/logos/158x48/statamic-logo-gray-900.svg"
-            alt="Statamic"
-            width={158}
-            height={48}
-          />
+        <div className="flex-column ">
+          <div className="flex justify-center ">
+            <div className="bg-[url('../public/images/Menu.gif')] h-[220px] w-full bg-no-repeat bg-contain bg-center top-10"></div>
+          </div>
         </div>
       </div>
     </div>
