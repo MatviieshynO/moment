0436751b5bf5bb4486349176e74772be19736a92
MatviieshynO <Olopromotion@gmail.com>
2023-05-05 02:26:45 +0200

    dasd

--- src/components/ArrowSection.jsx
+++ src/components/ArrowSection.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom'
 export default function ArrowSection() {
   return (
     <div className="bg-white">
@@ -16,10 +17,12 @@ export default function ArrowSection() {
         <div className="bg-[url('../public/images/arrowImage.gif')] h-[70px] w-40 bg-no-repeat bg-cover bg-center top-10"></div>
       </div>
       {/* /arrowSection */}
-      <div className=" pt-2 bg-[#D6EDF1] border-t-2 border-teal-300">
-        <h2 className="text-center text-lg leading-6 text-teal-800 font-mono pb-2">
-          У нас ви можете замовити
-        </h2>
+      <div className=" pt-2 bg-[#D6EDF1] border-t-2 border-teal-100">
+        <NavLink to="/menu" className="flex justify-center items-center">
+          <div className="w-[70%] text-center border-2 border-black rounded-sm hover:bg-teal-100">
+            Перейти до меню
+          </div>
+        </NavLink>
         <div className="flex-column ">
           <div className="flex justify-center ">
             <div className="bg-[url('../public/images/Menu.gif')] h-[220px] w-full bg-no-repeat bg-contain bg-center top-10"></div>
