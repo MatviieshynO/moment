e2d88b7cb9790f5d5e212f39136a62a13ab1d6ed
MatviieshynO <Olopromotion@gmail.com>
2023-05-07 18:25:43 +0200

    dasd

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -47,13 +47,6 @@ const Slider = () => {
           className="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
           aria-label="Slide 4"
         ></button>
-        <button
-          type="button"
-          data-te-target="#carouselExampleCaptions"
-          data-te-slide-to="4"
-          className="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
-          aria-label="Slide 5"
-        ></button>
       </div>
 
       {/* <!--Carousel items--> */}
