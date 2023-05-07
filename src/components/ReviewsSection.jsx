e188dca3599cc3688c1be1e65bcfe47b7540d3f6
MatviieshynO <Olopromotion@gmail.com>
2023-05-07 10:02:03 +0200

    dasd

--- src/components/ReviewsSection.jsx
+++ src/components/ReviewsSection.jsx
@@ -26,7 +26,7 @@ export default function RewiewsSection() {
             Напишіть відгук про нашу кав'ярню
           </h2>
           <p className="mt-2 text-lg leading-8 text-center text-gray-600">
-            Ми хочемо бути кращими, тому прислухаємося до своїх клієнтів
+            Ми хочемо бути кращими, тому прислуховуємось до наших клієнтів
           </p>
         </div>
         <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
