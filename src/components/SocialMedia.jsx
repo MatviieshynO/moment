646f2ffc2b3177fa0c79763d69771655c9eed2ad
MatviieshynO <Olopromotion@gmail.com>
2023-05-29 14:14:52 +0200

    dasd

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -4,19 +4,19 @@ import { MdWifiCalling3 } from 'react-icons/md'
 
 const SocialMedia = () => {
   return (
-    <div className="flex-column absolute top-[90px] left-[0] md:hidden bg-white rounded gsap-04">
+    <div className="flex-column absolute top-[58px] left-[-4px] md:hidden rounded bg-gradient-to-r from-[#092d42] via-[#005672] to-[#2683b1] gsap-04">
       <a
         href="https://www.instagram.com/moment.cv/"
         target="blank"
         className="bg-[red]"
       >
-        <AiFillInstagram className="cursor-pointer ml-[1px] text-2xl  rounded-full p-[3px] gsap-03" />
+        <AiFillInstagram className="cursor-pointer ml-[1px] text-3xl  p-[5px] gsap-03 bg-white" />
       </a>
       <a href="/" target="blank">
-        <GrMail className="cursor-pointer ml-[1px] text-2xl ] rounded-full p-[3px] gsap-03" />
+        <GrMail className="cursor-pointer ml-[1px] text-3xl p-[5px] gsap-03 " />
       </a>
       <a href="/" target="blank">
-        <MdWifiCalling3 className="cursor-pointer ml-[1px] text-2xl  rounded-full p-[3px] gsap-03" />
+        <MdWifiCalling3 className="cursor-pointer ml-[1px] text-3xl p-[5px] gsap-03 text-white" />
       </a>
     </div>
   )
